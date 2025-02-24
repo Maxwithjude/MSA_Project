e49314061e719e46f200a616a463b470929840b1
donghyeon <inews2432@gmail.com>
2025-02-24 20:50:48 +0900

    add

---
+++ yureka/src/test/java/com/example/yureka/YurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
